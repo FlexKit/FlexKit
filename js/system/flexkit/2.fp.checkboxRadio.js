bcaf99493a1b8f9f296fbfea88c6cc47b9cbff2c
Pavel Yashin <yashin.ziby@gmail.com>
2015-04-29 12:04:16 +0300

    Add check clone classes

--- js/system/flexkit/2.fp.checkboxRadio.js
+++ js/system/flexkit/2.fp.checkboxRadio.js
@@ -1,46 +1,46 @@
-function _checkboxRadio(){
-//    if(!$('.ie8').length){
-        $('input:checkbox, input:radio').not('.processed, .icon, .hidden').each(function(){
+function _checkboxRadio() {
+    //    if(!$('.ie8').length){
+    $('input:checkbox, input:radio').not('.processed, .icon, .hidden').each(function() {
 
         var id = $(this).prop('id'), labelClass = '';
-            if(!id){
-                id = 'chr-'+Math.floor((Math.random()*100000)+1);
-                $(this).prop('id', id);
-            }
+        if(!id) {
+            id = 'chr-' + Math.floor((Math.random() * 100000) + 1);
+            $(this).prop('id', id);
+        }
 
         if($(this).data('clone-classes') === 'true') {
             labelClass = $(this).prop('class') || '';
         }
 
-            $(this).addClass('processed');
+        $(this).addClass('processed');
 
-            if($(this).is(':radio')){
-                $(this).addClass('radio-upgrade filed-upgrade');
-            }else{
-                $(this).addClass('checkbox-upgrade filed-upgrade');
+        if($(this).is(':radio')) {
+            $(this).addClass('radio-upgrade filed-upgrade');
+        } else {
+            $(this).addClass('checkbox-upgrade filed-upgrade');
+        }
+
+        if(!$(this).closest('.btn-group').length) {
+            var $parent = $(this).parent('label');
+            var $span = $('<span class="checkbox_radio"></span>');
+
+            if($(this).hasClass('switcher')) {
+                $span.append('<span></span>');
             }
 
-            if(!$(this).closest('.btn-group').length){
-                var $parent = $(this).parent('label');
-                var $span = $('<span class="checkbox_radio"></span>');
-
-                if($(this).hasClass('switcher')){
-                    $span.append('<span></span>');
-                }
-
-                if($parent.length){
-                    $parent.prop('for', id);
-                    $span.addClass(labelClass);
-                }else{
-                    var $label = $('<label></label>');
-                    $label.addClass('checkbox_radio-wrap pointer ' + labelClass).prop('for', id);
-                    $(this).wrap($label);
-                }
-
-                $(this).after($span);
+            if($parent.length) {
+                $parent.prop('for', id);
+                $span.addClass(labelClass);
+            } else {
+                var $label = $('<label></label>');
+                $label.addClass('checkbox_radio-wrap pointer ' + labelClass).prop('for', id);
+                $(this).wrap($label);
             }
-        });
-//    }
+
+            $(this).after($span);
+        }
+    });
+    //    }
 }
 _checkboxRadio();
 
