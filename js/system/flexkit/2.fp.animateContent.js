87e03a2d1717bba8eb70ab05d12d8832441f9d8f
PavelCSS <yashin.ziby@gmail.com>
2015-05-25 10:37:51 +0300

    Add functionality for animation content

--- js/system/flexkit/2.fp.animateContent.js
+++ js/system/flexkit/2.fp.animateContent.js
@@ -6,21 +6,18 @@ $(window).load(function(){
     var $scrollBox = $('.scroll-effect');
 
     if($scrollBox.length){
+
+        var scrollBoxPos = [];
         $scrollBox.each(function(i){
-            blockPos[i] = $(this).offset().top + 75;
+            scrollBoxPos[i] = $(this).offset().top + 75;
         });
         scrollAnimate();
-    }
-
-    if($scrollBox.length){
-
-        var blockPos = [];
 
         function scrollAnimate(){
             var scrollTop = $(window).scrollTop() + $(window).height();
-            for(var i = 0; i<=blockPos.length; i++){
+            for(var i = 0; i<=scrollBoxPos.length; i++){
 
-                if(scrollTop>=blockPos[i]){
+                if(scrollTop>=scrollBoxPos[i]){
                     $scrollBox.eq(i).addClass('animated');
                 }
             }
@@ -32,7 +29,7 @@ $(window).load(function(){
 
         $(window).resize(function(){
             $scrollBox.each(function(i){
-                blockPos[i] = $(this).offset().top + 75;
+                scrollBoxPos[i] = $(this).offset().top + 75;
             });
         });
     }
