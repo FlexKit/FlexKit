72bc60fe2667199fc6edf68ad8ed505d82aae72a
Pavel Yashin <yashin.ziby@gmail.com>
2015-09-18 17:52:03 +0300

    Fix chosen select

--- js/system/flexkit-components/2.fp.chosen.js
+++ js/system/flexkit-components/2.fp.chosen.js
@@ -9,28 +9,32 @@ function _chosen() {
             allow_single_deselect : true
         });
 
+        // Don't hide dropdown after selection on a multiple select
+        // https://github.com/harvesthq/chosen/issues/2310#issuecomment-93372444
         var chosen = $(".chosen-select.chosen-dropdownshow").data('chosen');
-        var autoClose = false;
-        var chosen_resultSelect_fn = chosen.result_select;
-        chosen.search_contains = true;
-        chosen.result_select = function(evt) {
-            var resultHighlight = null;
-            if(autoClose === false) {
-                evt['metaKey'] = true;
-                evt['ctrlKey'] = true;
-                resultHighlight = chosen.result_highlight;
-            }
-            var stext = chosen.get_search_text();
-            var result = chosen_resultSelect_fn.call(chosen, evt);
-            if(autoClose === false && resultHighlight !== null)
-                resultHighlight.addClass('result-selected');
+        if(chosen) {
+            var autoClose = false;
+            var chosen_resultSelect_fn = chosen.result_select;
+            chosen.search_contains = true;
+            chosen.result_select = function(evt) {
+                var resultHighlight = null;
+                if(autoClose === false) {
+                    evt['metaKey'] = true;
+                    evt['ctrlKey'] = true;
+                    resultHighlight = chosen.result_highlight;
+                }
+                var stext = chosen.get_search_text();
+                var result = chosen_resultSelect_fn.call(chosen, evt);
+                if(autoClose === false && resultHighlight !== null)
+                    resultHighlight.addClass('result-selected');
 
-            this.search_field.val(stext);
-            this.winnow_results();
-            this.search_field_scale();
+                this.search_field.val(stext);
+                this.winnow_results();
+                this.search_field_scale();
 
-            return result;
-        };
+                return result;
+            };
+        }
     }
 }
 _chosen();
