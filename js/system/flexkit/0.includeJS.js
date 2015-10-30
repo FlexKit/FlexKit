c8aab8c67e8d240834432df14d65e1f8a6905066
Pavel Yashin <yashin.ziby@gmail.com>
2015-10-30 11:17:31 +0200

    Fix include files

--- js/system/flexkit/0.includeJS.js
+++ js/system/flexkit/0.includeJS.js
@@ -1,18 +1,18 @@
 (function() {
-    var url = document.querySelector('[href*="themes"]').getAttribute('href'),
-        websiteUrl = url.substring(url.indexOf('themes', 10), 0),
-        themeIndex = url.indexOf('themes') + 7,
-        themeName = url.substring(themeIndex, url.indexOf('/', themeIndex));
+    //var url = document.querySelector('[href*="themes"]').getAttribute('href'),
+    //    websiteUrl = url.substring(url.indexOf('themes', 10), 0),
+    //    themeIndex = url.indexOf('themes') + 7,
+    //    themeName = url.substring(themeIndex, url.indexOf('/', themeIndex));
 
-    if(!jQuery().carousel) {
-        loadScript(websiteUrl + "themes/" + themeName + "/js/plugin/jquery.carousel.min.js");
+    if(window.jQuery && !jQuery().carousel) {
+        loadScript("/js/plugin/jquery.carousel.min.js");
     }
 
-    if(!jQuery().chosen && document.getElementsByClassName("chosen-select").length || document.getElementsByClassName("chosen-select-deselect").length) {
-        loadScript(websiteUrl + "/themes/" + themeName + "/js/plugin/jquery.chosen.min.js");
+    if(window.jQuery && !jQuery().chosen && document.getElementsByClassName("chosen-select").length || document.getElementsByClassName("chosen-select-deselect").length) {
+        loadScript("/js/plugin/jquery.chosen.min.js");
     }
 
     if(!window.Hammer && device.type) {
-        loadScript(websiteUrl + "/themes/" + themeName + "/js/plugin/hammer.min.js");
+        loadScript("/js/plugin/hammer.min.js");
     }
 })();
